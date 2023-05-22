c9d58efe1341e0dfd387f2e7e48ec721d0733567
DaksheshRao <dakshesh.rao@cubastion.com>
2023-05-22 18:42:20 +0530

    fibonacci file

---
+++ fibonacci.sh
@@ -0,0 +1,17 @@
+#!  /bin/bash
+n=10
+a=0
+b=1
+c=2
+echo "Fibonnaci Series : "
+echo $a
+echo $b
+while [ $c -le $n ]
+	do
+		fib=`expr $a + $b`
+		a=$b
+		b=$fib
+		echo $fib
+		c=`expr $c + 1`
+	done
+sleep infinity
